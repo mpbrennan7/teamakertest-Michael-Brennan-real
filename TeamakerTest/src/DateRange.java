8d92402da1be43bc23bd336153ec14524d3c29b3
brennanm <brennanm@R416-10-780.Fontbonne.edu>
2019-03-14 15:16:40 -0500

    "Completed activities 1-4"

---
+++ TeamakerTest/src/DateRange.java
@@ -0,0 +1,39 @@
+import java.util.Date;
+
+public class DateRange {
+	private Date start;
+	private Date end;
+
+	public DateRange(Date start, Date end) {
+		this.setStart(start);
+		this.setEnd(end);
+	}
+
+	/**
+	 * @return the start
+	 */
+	public Date getStart() {
+		return start;
+	}
+
+	/**
+	 * @param start the start to set
+	 */
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	/**
+	 * @return the end
+	 */
+	public Date getEnd() {
+		return end;
+	}
+
+	/**
+	 * @param end the end to set
+	 */
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+}
